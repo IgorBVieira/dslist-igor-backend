3b18dfb437c07e2ad4aed604bd44198d14b7403a
Igor Bernardo Vieira <112185640+IgorBVieira@users.noreply.github.com>
2023-11-19 11:56:17 -0300

    initial project

---
+++ src/test/java/com/devigor/dslist/DslistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devigor.dslist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DslistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
